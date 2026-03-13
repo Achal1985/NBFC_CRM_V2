a194f911dd4044a252eb2dbd561e09be3a69e40a
Achal1985 <pradhanachal70@gmail.com>
2026-03-13 16:09:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ import pandas as pd
 
 app = Flask(__name__)
 
-file_path = "C:/CRM_PROJECT/NBFC_CRM_Windows11_Chatbot.xlsx"
+import os
+
+# Use relative path
+file_path = os.path.join(os.path.dirname(__file__), "NBFC_CRM_Windows11_Chatbot.xlsx")
 
 customer_df = pd.read_excel(file_path, sheet_name="Customer_Master")
 loan_df = pd.read_excel(file_path, sheet_name="Loan_Details")
