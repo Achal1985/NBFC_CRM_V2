deb2d476e066a0a480942ef6c936af5fdf25c607
Achal1985 <pradhanachal70@gmail.com>
2026-03-13 16:01:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,10 @@ loan_df = pd.read_excel(file_path, sheet_name="Loan_Details")
 @app.route("/")
 def home():
     return render_template("index.html")
-
+    
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 @app.route("/search", methods=["POST"])
 def search():
